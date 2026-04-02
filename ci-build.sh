221cd5c8328c1750ddc129e84b7ef20ef0796702
Pinchuk Kyrylo <pinchukkg@gmail.com>
2026-04-02 23:55:56 +0300

    Working on dev prod scripts

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-OWNER="other-user" 
+OWNER="hexyatina"
 REPO="backend-app"
 export IMAGE_NAME="ghcr.io/$OWNER/$REPO:local-test"
 
@@ -13,18 +13,17 @@ docker compose -f docker/docker-compose.backend.yaml up -d
 
 MAX_RETRIES=15
 COUNT=0
-until curl -s -f http://localhost:5000/ > /dev/null || [ $COUNT -eq $MAX_RETRIES ]; do
-    echo "Wait... ($((COUNT+1))/$MAX_RETRIES)"
+until curl -s -f http://localhost:5000/ > /dev/null || 2>&1; do
+    if [ "$COUNT" -ge "$MAX_RETRIES" ]; then
+        echo "Smoke test failed after $MAX_RETRIES attempts!"
+        docker compose -f docker/docker-compose.backend.yaml logs backend
+        docker compose -f docker/docker-compose.backend.yaml down
+        exit 1
+    fi
+    echo "Waiting... ($((COUNT+1))/$MAX_RETRIES)"
     sleep 3
     COUNT=$((COUNT+1))
 done
-
-if [ $COUNT -eq $MAX_RETRIES ]; then
-    echo "Smoke test failed!"
-    docker compose -f docker/docker-compose.backend.yaml logs backend
-    docker compose -f docker/docker-compose.backend.yaml down
-    exit 1
-fi
 
 echo "Smoke test passed!"
 docker compose -f docker/docker-compose.backend.yaml down
